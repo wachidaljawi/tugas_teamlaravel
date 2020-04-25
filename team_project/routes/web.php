35adbd1911ef645d782d71d8849a0e83cacd0ec7
rizaldieqy <rizalasshidieqy09@gmail.com>
2020-04-25 16:14:52 +0700

    template thr done

--- team_project/routes/web.php
+++ team_project/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dataTHR', function(){
+    return view('data.thr');
+})->name('thr');
